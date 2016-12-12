75b97f8c2fa1acae2f428e4ae41595438dbaf895
Frank La Vigne <frankchevy@hotmail.com>
2016-12-12 13:51:57 -0500

    ADded User control

--- TagSearcherUWP/MainPage.xaml.cs
+++ TagSearcherUWP/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.Services.Twitter;
+//using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +27,59 @@ namespace TagSearcherUWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+
+        private async void TwitterLogin()
+        {
+
+            TwitterService.Instance.Initialize("", "", "");
+
+            // Login to Twitter
+            if (!await TwitterService.Instance.LoginAsync())
+            {
+                return;
+            }
+
+            // Get current user info
+            var user = await TwitterService.Instance.GetUserAsync();
+            //ProfileImage.DataContext = user;
+
+            // Get user timeline
+            //ListView.ItemsSource = await TwitterService.Instance.GetUserTimeLineAsync(user.ScreenName, 50);
+
+            // Post a tweet
+            await TwitterService.Instance.TweetStatusAsync("");
+
+            // Post a tweet with a picture
+            //await TwitterService.Instance.TweetStatusAsync(TweetText.Text, stream);
+
+            // Search for a specific tag
+            //ListView.ItemsSource = await TwitterService.Instance.SearchAsync(TagText.Text, 50);
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //BladeItem bi = new BladeItem();
+            //bi.Title = "Yay";
+
+            //bladeView.Items.Add(bi);
+
+            PivotItem pi = new PivotItem();
+            pi.Header = "Yay!";
+
+            pi.Content = new SearchResults();
+
+            this.pvtSearches.Items.Add(pi);
+
+
+
         }
     }
 }
